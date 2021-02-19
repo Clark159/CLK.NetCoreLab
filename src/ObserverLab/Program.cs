64505a930eed19abf57e16d50e232490b7da5f5d
clark159 <clark159@hotmail.com>
2021-02-20 06:22:03 +0800

    Add ObserverLab

---
+++ src/ObserverLab/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace ObserverLab
+{
+    public class Program
+    {
+        // Methods
+        public static void Main(string[] args)
+        {
+            // Publisher
+            using (var locationPublisher = new LocationPublisher())
+            {
+                // Observer
+                var locationObserver = new LocationObserver();
+
+                // Subscribe
+                var locationObserverUnsubscriber = locationPublisher.Subscribe(locationObserver);
+                locationPublisher.Write(new Location() { X = 1, Y = 1 });
+                locationPublisher.Write(new Location() { X = 2, Y = 2 });
+
+                // Unsubscribe
+                locationObserverUnsubscriber.Dispose();
+                locationPublisher.Write(new Location() { X = 3, Y = 3 });
+            }
+        }
+    }
+
+    public class Location
+    {
+        // Properties
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+
+    public class LocationPublisher : Observable<Location>
+    {
+
+    }
+
+    public class LocationObserver : IObserver<Location>
+    {
+        // Methods
+        public void OnNext(Location location)
+        {
+            #region Contracts
+
+            if (location == null) throw new ArgumentException(nameof(location));
+
+            #endregion
+
+            // Display
+            Console.WriteLine($"Location: X={location.X}, Y={location.Y}");
+        }
+
+        public void OnCompleted()
+        {
+            // Nothing
+
+        }
+
+        public void OnError(Exception error)
+        {
+            #region Contracts
+
+            if (error == null) throw new ArgumentException(nameof(error));
+
+            #endregion
+
+            // Nothing
+
+        }
+    }
+}
---
+++ src/ObserverLab/Utilities/Observable.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverLab
+{
+    public class Observable<T> : IObservable<T>, IDisposable
+    {
+        // Fields
+        private readonly object _syncRoot = new object();
+
+        private List<IObserver<T>> _observerList = new List<IObserver<T>>();
+
+        private List<IObserver<T>> _observerListCache = null;
+
+
+        // Constructors
+        public Observable()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            // ObserverList
+            List<IObserver<T>> observerList = null;
+
+            // Sync
+            lock (_syncRoot)
+            {
+                // Require
+                if (_observerList == null) return;
+
+                // Clear
+                observerList = _observerList;
+                _observerList = null;
+
+                // Cache
+                _observerListCache = null;
+            }
+
+            // Notify
+            foreach (var observer in observerList)
+            {
+                // OnCompleted
+                observer.OnCompleted();
+            }
+        }
+
+
+        // Methods
+        public void Write(T value)
+        {
+            // ObserverList
+            var observerList = _observerListCache;
+            if (observerList == null)
+            {
+                // Sync
+                lock (_syncRoot)
+                {
+                    // Require
+                    if (_observerList == null) throw new ObjectDisposedException(this.GetType().Name);
+
+                    // Cache
+                    if (_observerListCache == null)
+                    {
+                        _observerListCache = _observerList.ToList();
+                    }
+                    observerList = _observerListCache;
+                }
+            }
+
+            // Notify
+            foreach (var observer in observerList)
+            {
+                // OnNext
+                observer.OnNext(value);
+            }
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            #region Contracts
+
+            if (observer == null) throw new ArgumentException(nameof(observer));
+
+            #endregion
+
+            // Sync
+            lock (_syncRoot)
+            {
+                // Require
+                if (_observerList == null) throw new ObjectDisposedException(this.GetType().Name);
+
+                // Add
+                if (_observerList.Contains(observer) == false)
+                {
+                    _observerList.Add(observer);
+                }
+
+                // Cache
+                _observerListCache = null;
+            }
+
+            // Return
+            return new Unsubscriber(() => this.Unsubscribe(observer));
+        }
+
+        private void Unsubscribe(IObserver<T> observer)
+        {
+            #region Contracts
+
+            if (observer == null) throw new ArgumentException(nameof(observer));
+
+            #endregion
+
+            // Sync
+            lock (_syncRoot)
+            {
+                // Require
+                if (_observerList == null) return;
+
+                // Remove
+                if (_observerList.Contains(observer) == true)
+                {
+                    _observerList.Remove(observer);
+                }
+
+                // Cache
+                _observerListCache = null;
+            }
+        }
+
+
+        // Class
+        private class Unsubscriber : IDisposable
+        {
+            // Fields
+            private readonly Action _removeAction = null;
+
+
+            // Constructors
+            public Unsubscriber(Action removeAction)
+            {
+                #region Contracts
+
+                if (removeAction == null) throw new ArgumentException(nameof(removeAction));
+
+                #endregion
+
+                // Default
+                _removeAction = removeAction;
+            }
+
+            public void Dispose()
+            {
+                // Execute
+                _removeAction();
+            }
+        }
+    }
+}
