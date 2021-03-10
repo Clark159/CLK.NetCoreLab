d6ce20396d199374be51fb2f153374ce48594123
clark159 <clark159@hotmail.com>
2021-03-11 01:37:33 +0800

    Add AutoActivateLab

---
+++ src/AutoActivateLab/Program.cs
@@ -0,0 +1,66 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoActivateLab
+{
+    public class Program
+    {
+        // Methods
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<Autofac.ContainerBuilder>((containerBuilder) =>
+                {
+                    // SettingContext
+                    {
+                        // Register
+                        containerBuilder.RegisterType<SettingContext>().As<SettingContext>()
+
+                        // Start
+                        .OnActivated(handler =>
+                        {
+                            handler.Instance.Start();
+                        })
+
+                        // Lifetime
+                        .AutoActivate().SingleInstance();
+                    }
+                })
+            ;
+        }
+
+
+        // Class
+        public class SettingContext
+        {
+            // Constructors
+            public SettingContext()
+            {
+                // Display
+                Console.WriteLine("SettingContext Created");
+            }
+
+
+            // Methods
+            public void Start()
+            {
+                // Execute
+                Console.WriteLine("SettingContext Started");
+            }
+        }
+    }
+}
